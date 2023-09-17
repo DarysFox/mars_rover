de60b19e68d73ec9be047ab5fc5080ae41ee7281
DarysFox <darya.khsv@gmail.com>
2023-09-17 16:13:31 +0300

    roverData

--- src/main/java/com/MarsRoverApiService.java
+++ src/main/java/com/MarsRoverApiService.java
@@ -1,7 +1,15 @@
-@Service
-
 import org.springframework.stereotype.Service;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+@Service
 
 public class MarsRoverApiService{
 
+    public getRoverData(){
+        RestTemplate rt = new RestTemplate();
+
+        ResponseEntity<MarsRoverApiResponse> response = rt.getForEntity("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=DEMO_KEY", MarsRoverApiService);
+        return response.getBody();
+
+    }
 }
--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -1,17 +1,17 @@
 package com.example.demo;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-
-
 
 @Controller
 public class HomeController {
-    
+
+    @Autowired
+    private MarsRoverApiService roverService;
+
     @GetMapping("/")
     public String getHomeView(ModelMap model){
-        
+        MarsRoverApiResponse roverData = roverService.getRoverData();
+        model.put("roverData", roverData);
         return "index";
     }
 }
