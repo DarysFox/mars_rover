9800fb2c4ce1b29fc701a6e0f6ee0c4040474f66
DarysFox <darya.khsv@gmail.com>
2023-09-17 16:22:31 +0300

    photo

--- src/main/java/com/MarsRoverApiService.java
+++ src/main/java/com/MarsRoverApiService.java
@@ -1,14 +1,15 @@
 import org.springframework.stereotype.Service;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
-@Service
 
+@Service
 public class MarsRoverApiService{
 
     public getRoverData(){
         RestTemplate rt = new RestTemplate();
 
         ResponseEntity<MarsRoverApiResponse> response = rt.getForEntity("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=DEMO_KEY", MarsRoverApiService);
+        
         return response.getBody();
 
     }
